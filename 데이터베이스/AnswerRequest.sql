729381b9cc1ea6ad916fffaffbf653e45b3017bb
klarnuri <klarnuri@gmail.com>
2024-08-18 02:36:04 +0900

    0814 견적서 답변, 답변용 테이블, dto, mapper 작성중

---
+++ 데이터베이스/AnswerRequest.sql
@@ -0,0 +1,11 @@
+create table AnswerRequest
+(
+    no        int primary key auto_increment,
+    RequestNo int         not null,
+    type      varchar(20) not null,
+    GosuId    varchar(20) not null,
+    price     varchar(50),
+    ref       varchar(1000),
+    file      varchar(2000)
+)
+;
